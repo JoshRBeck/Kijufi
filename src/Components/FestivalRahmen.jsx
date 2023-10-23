cdd63bea991c70a0e9c607af36b80a1ab8d8ead6
beckjosh <Joshbeck17@gmail.com>
2023-10-23 21:07:30 +0200

    "Lots of font size editing"

--- src/Components/FestivalRahmen.jsx
+++ src/Components/FestivalRahmen.jsx
@@ -1,9 +1,23 @@
+import React from 'react';
+
 function FestivalRahmen() {
   return (
-    <div className="flex flex-col m-10  container mx-auto px-10 text-center tracking-normal break-all rounded-3xl bg-[#EFF6F4]">
-      <h2 className="pt-10 pb-10 font-medium tracking-normal break-all text-2xl sm:text-3xl md:text-2xl lg:text-xl xl:text-xl">RAHMENPROGRAMM & MEHR</h2>
-      <p className="pb-20 pl-10 pr-10 text-justify tracking-normal break-alltext-2xl sm:text-lg md:text-md lg:text-sm xl:text-sm">
-        Die Festival-Vorbereitungen laufen auf Hochtouren. Mehr Infos zur Festivalparty, zum Catering und weiteren Events im Begleitprogramm könnt ihr spätestens Ende November hier nachlesen. Ihr kommt aus Berlin und habt Lust, als Mitglied der Crew das Festival mitzugestalten. Dann lest weiter unten, wie ihr das tun könnt.
+    <div className="flex flex-col m-10 container mx-auto px-10 text-center tracking-normal break-all rounded-3xl bg-[#EFF6F4]">
+      <h2 className="pt-10 pb-10 font-bold tracking-normal break-all text-[22.24px] md:text-[40px] relative pl-2">
+        <span className="text-[#323A64] relative">
+          RAHMENPROGRAMM & MEHR
+        </span>
+        <span className="absolute -inset-4 -z-10 text-white">
+          RAHMENPROGRAMM & MEHR
+        </span>
+        <span className="absolute left-1 top-0.5 text-outline text-white pl-2">
+          RAHMENPROGRAMM & MEHR
+        </span>
+      </h2>
+      <p className="pb-20 pl-10 pr-10 text-justify whitespace-normal break-normal text-[13.9] md:text-[25px]" >
+        Die Festival-Vorbereitungen laufen auf Hochtouren. Mehr Infos zur Festivalparty, zum Catering und weiteren Events im Begleitprogramm könnt ihr spätestens Ende November hier nachlesen.
+        <br /><br />
+        Ihr kommt aus Berlin und habt Lust, als Mitglied der Crew das Festival mitzugestalten. Dann lest weiter unten, wie ihr das tun könnt.
       </p>
     </div>
   );
--- src/Pages/Program.jsx
+++ src/Pages/Program.jsx
@@ -7,10 +7,10 @@ import Platzhalter from '../Components/Platzhalter';
 function Program() {
 	return <div><div>
 		<div className="p-10 flex justify-center align-middle items-center flex-col">
-			<h1 className="font-semibold text-[#323A64] text-center p-10 text-4xl halcyon-thin">
+			<h1 className="font-semibold text-[#323A64] text-center p-10 text-[20px] md:text-[50px] halcyon-thin">
 				Unser Programm für Filmemacher
 			</h1>
-			<p className="flex justify-center items-center text-2xl bg-[#F1F7F6] w-fit text-center rounded-full">
+			<p className="flex justify-center items-center text-[11.3px] md:text-[25px] bg-[#F1F7F6] w-fit text-center rounded-full">
 				🕘 : Startzeit 🎬 : Filmvorführungen 🥳 : Festival 📍 : Standort
 			</p>
 		</div>
