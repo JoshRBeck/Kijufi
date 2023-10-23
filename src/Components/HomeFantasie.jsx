a5ec102423749a9db72c7ce30d46cbdc4c9bc837
AlexMaunouryBenavides <alexandremaunoury@yahoo.fr>
2023-10-23 17:05:02 +0200

    fix for homepage

--- src/Components/HomeFantasie.jsx
+++ src/Components/HomeFantasie.jsx
@@ -2,13 +2,13 @@ import cookList from '../Assets/cooking-list.png';
 
 function HomeFantasie() {
 	return (
-		<section className="flex items-center justify-between pt-5 md:h-[400px] md:w-[1300px] md:m-auto">
+		<section className="flex items-center justify-center md:gap-10 pt-5 md:h-[500px] md:m-auto">
 			<div>
 				<img className="h-[200px] md:h-full" src={cookList} alt="cooking-list" />
 			</div>
 			<div className="w-[250px] md:w-[500px]">
-				<h3 className="text-[20px] font-bold md:text-[35px]"> #Fantasie #Miteinander</h3>
-				<p className="mt-5 md:text-[20px]">Was wir brauchen ist: Zusammenhalt, Miteinander und viel Fantasie. Also weg mit den Grenzen im Kopf.</p>
+				<h3 className="text-[20px] font-bold md:text-[40px]"> #Fantasie #Miteinander</h3>
+				<p className="mt-5 md:text-[25px]">Was wir brauchen ist: Zusammenhalt, Miteinander und viel Fantasie. Also weg mit den Grenzen im Kopf.</p>
 			</div>
 		</section>
 	);
--- src/Components/HomeInfos.jsx
+++ src/Components/HomeInfos.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import Btn from './Btn';
 function HomeInfos() {
 	return (
 		<section className="bg-[#D5E7E3] h-fit md:h-[650px] py-10 text-center md:flex flex-col items-center justify-center gap-7">
-			<h3 className="text-[50px] font-bold ">Über das Festival</h3>
+			<h3 className="text-[50px] font-semibold ">Über das Festival</h3>
 			<p className="text-[25px] md:w-[60%] mt-4">
 				Gemeinsam Grenzenlos ist ein Film-Festival für alle Generationen: Junge Filmemacher:innen sind eingeladen, mit ihren Ideen und Überzeugungen ins Spotlight zu treten. Sagt, was ihr zu sagen habt. Zeigt, was euch bewegt und was euch wichtig ist! Auch ohne Film sind Kinder und Jugendliche eingeladen, als Zuschauer:innen Teil eines vibrierenden Festivalerlebnisses zu werden. Trefft auf engagierte Gleichaltrige. Lasst euch inspirieren. Alle Erwachsenen! Kommt – denn ihr seid genauso eingeladen.
 				Ihr seid der Teil von Gemeinsam, der Grenzen setzen, aber auch loswerden kann.
 			</p>
 			<p className="text-[30px] font-bold mt-6 ">⚠️ Achtung! Der Ticketverkauf startet Anfang November 2023</p>
-			<button type="button" className="py-[11px] px-[30px] text-black bg-white rounded-[30px] mt-4 shadow-[3px_3px_2px_-1px_rgba(0,0,0,0.9)] ">
-				<NavLink to={'program'}>Programm</NavLink>
-			</button>
+
+			<Btn text={'Programm'} backgroundColor="bg-white" hoverColor="hover:bg-[#5BA092]" />
 		</section>
 	);
 }
