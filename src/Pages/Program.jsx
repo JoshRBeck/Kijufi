5f90e500af643ea7fbf1cdcaea337d170ce9f728
beckjosh <Joshbeck17@gmail.com>
2023-11-06 04:27:40 +0100

    "Program and info button adjusted"

--- src/Pages/Program.jsx
+++ src/Pages/Program.jsx
@@ -8,17 +8,16 @@ import Btn from '../Components/Btn';
 function Program() {
 	return (
 		<div>
+			<section id="#scrollToMe">
+				<TeilFestival />
+			</section>
+			<FestivalRahmen />
+			<Platzhalter />
 			<div className="pt-32 pl-5 md:text-center">
 				<h1 className=" font-semibold text-[#323A64]  text-[20px]  halcyon-thin md:text-[50px]">Unser Programm für Filmemacher</h1>
 				<p className=" w-[200px] p-4 mt-2 text-[16px] text-[#333C3A] bg-[#F1F7F6] rounded-[15px] md:text-[25px] md:m-auto md:w-fit ">🕘 : Startzeit 🎬 : Filmvorführungen 🥳 : Festival 📍 : Standort</p>
 			</div>
 			<Schedule />
-
-			<FestivalRahmen />
-			<section id="#scrollToMe">
-			<TeilFestival />
-			</section>
-			<Platzhalter />
 		</div>
 	);
 }
