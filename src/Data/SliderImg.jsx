f1f34382574feb6199861b98de5a060939e17a47
beckjosh <Joshbeck17@gmail.com>
2023-11-08 09:39:40 +0100

    "added sponsors"

--- src/Data/SliderImg.jsx
+++ src/Data/SliderImg.jsx
@@ -8,12 +8,36 @@ import mpower from '../Assets/SliderImg/logo-mpower.png';
 import prisod from '../Assets/SliderImg/logo-prisod.png';
 import sbm from '../Assets/SliderImg/logo-sbm.png';
 import yorck from '../Assets/SliderImg/logo-yorck.png';
+import global from "../Assets/SliderImg/Global We Media_cropped-Frame-1-5-1024x292.png"
+import Joli from "../Assets/SliderImg/Joliba_Logo_Ohne Schrift und Hintergrund.png"
+import konigstadt from "../Assets/SliderImg/logo_koenigstadt_2013_dl.png"
+import kultur from "../Assets/SliderImg/Logo_Kulturplakatierung_pink_2021.png"
 
 export const SliderImgs = [
 	{
 		link: 'https://www.gs-am-buergerpark.de/',
 		image: grundschule,
 		alt: 'grundschule',
+	},
+	{
+		link: 'https://www.kulturplakatierung.de/?gclid=Cj0KCQiAgK2qBhCHARIsAGACuzm5AA_9XLYwl4z_RUe-wFNZllyspLJVXxnyNk9GERYUgpDcSqv93K4aAm8OEALw_wcB',
+		image: kultur,
+		alt: 'kultur plakatierung'
+	},
+	{
+		link: 'https://www.jugendhaus-koenigstadt.de/',
+		image: konigstadt,
+		alt: 'king city'
+	},
+	{
+		link: 'https://global-we-media.com/',
+		image: global,
+		alt: 'global'
+	},
+	{
+		link: 'https://www.joliba.de/',
+		image: Joli,
+		alt: 'joliba'
 	},
 	{
 		link: 'https://www.leo-lionni-grundschule.de/',
